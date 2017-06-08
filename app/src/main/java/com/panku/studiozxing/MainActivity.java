55d87cb1e2771f17e2c25e73c16077e157c3690a
hyn0525 <hyn_0525@sina.com>
2017-06-08 16:08:34 +0800

    Initial commit

---
+++ app/src/main/java/com/panku/studiozxing/MainActivity.java
@@ -0,0 +1,44 @@
+package com.panku.studiozxing;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                // 设置要扫描的条码类型，ONE_D_CODE_TYPES：一维码，QR_CODE_TYPES-二维码
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setCaptureActivity(ScanActivity.class);
+                integrator.setPrompt("请扫描二维码"); //底部的提示文字，设为""可以置空
+                integrator.setCameraId(0); //前置或者后置摄像头
+                integrator.setBeepEnabled(true); //扫描成功的「哔哔」声，默认开启
+                integrator.setBarcodeImageEnabled(true);
+                integrator.initiateScan();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (scanResult != null) {
+            String result = scanResult.getContents();
+            Log.e("HYN", result);
+            Toast.makeText(MainActivity.this, result, Toast.LENGTH_LONG).show();
+        }
+    }
+}
---
+++ app/src/main/java/com/panku/studiozxing/ScanActivity.java
@@ -0,0 +1,19 @@
+package com.panku.studiozxing;
+
+import android.os.Bundle;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+/**
+ * Date：2017/6/8
+ * Time: 15:52
+ * author: huangyanan
+ * 设置竖屏的过度界面
+ */
+
+public class ScanActivity extends CaptureActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
